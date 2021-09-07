c7ffbe3f46109bd9c831f28d11221cbe208c1c38
Tran Nhat Quang <tran67666@gmail.com>
2021-09-07 16:17:31 +0700

    make it better

---
+++ src/main/webapp/MyJS.js
@@ -0,0 +1,30 @@
+function NhiemVu1()
+{
+	var mess = document.getElementById("BT1");
+	mess.innerHTML="<h3>Mission</h3><p>Quang: using only JSP and deloy</p><p>Nguyên:Using only Servlet</p><p>Phát: Using Combine</p>";
+}
+function NhiemVu2()
+{
+	var mess = document.getElementById("BT1");
+	mess.innerHTML="<h3>Mission</h3><p>Quang: using only JSP and deloy</p><p>Nguyên:Using only Servlet</p><p>Phát: Using Combine</p>";
+}
+function NhiemVu3()
+{
+	var mess = document.getElementById("BT1");
+	mess.innerHTML="<h3>Mission</h3><p>Quang: using only JSP and deloy</p><p>Nguyên:Using only Servlet</p><p>Phát: Using Combine</p>";
+}
+function NhiemVu4()
+{
+	var mess = document.getElementById("BT1");
+	mess.innerHTML="<h3>Mission</h3><p>Quang: using only JSP and deloy</p><p>Nguyên:Using only Servlet</p><p>Phát: Using Combine</p>";
+}
+function NhiemVu5()
+{
+	var mess = document.getElementById("BT1");
+	mess.innerHTML="<h3>Mission</h3><p>Quang: using only JSP and deloy</p><p>Nguyên:Using only Servlet</p><p>Phát: Using Combine</p>";
+}
+function Xoa()
+{
+	var mess = document.getElementById("BT1");
+	mess.innerHTML="";	
+}
---
+++ src/main/webapp/exercises/exercise1.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Exercise Number One</title>
+</head>
+<body>
+	<center><h1>Exercise Number One</h1></center>
+	<ul>
+		<ol><h2><a href="">using only JSP</a></h2></ol>
+		<ol><h2><a href="">using only Servlet </a></h2></ol>
+		<ol><h2><a href="">combining JSP and Servlet in MVC framework</a></h2></ol>
+	</ul>
+</body>
+</html>
