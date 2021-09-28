3d6d7ef41837d3cea96002ca3469e260bae1db18
Trần Nhất Quang <tran67666@gmail.com>
2021-09-28 20:28:19 +0700

    make it better

--- src/main/java/com/Exercises/HomeShop.java
+++ src/main/java/com/Exercises/HomeShop.java
@@ -1,7 +1,6 @@
 package com.Exercises;
 
 import java.io.IOException;
-import java.net.URL;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,8 +9,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import javax.websocket.Session;
-
 import com.CookieControl.CookieUtils;
 import com.Model.Users;
 
