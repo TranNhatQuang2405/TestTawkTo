9bde84d4746e95df2abac7950a30d836b5a7a5d8
Trần Nhất Quang <tran67666@gmail.com>
2021-10-12 19:21:32 +0700

    make it better

--- src/main/webapp/style/MyJS.js
+++ src/main/webapp/style/MyJS.js
@@ -16,7 +16,7 @@ function NhiemVu3()
 function NhiemVu4()
 {
 	var mess = document.getElementById("BT1");
-	mess.innerHTML="<h3>Mission</h3><p>Nothing</p><h3>Status</h3><p>Nothing</p>";
+	mess.innerHTML="<h3>Mission</h3><p>Add To Cart Using With JSTL</p><h3>Status</h3><p>Finished</p>";
 }
 function NhiemVu5()
 {
